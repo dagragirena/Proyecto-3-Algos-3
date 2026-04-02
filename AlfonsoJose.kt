622aa4d1ca8768a82a5b9c2ed11cc69712b75ae4
soyvistorrr <victormhernandeza3009@gmail.com>
2026-04-01 23:45:50 -0400

    implementaciones
    Signed-off-by: soyvistorrr <victormhernandeza3009@gmail.com>

--- AlfonsoJose.kt
+++ AlfonsoJose.kt
@@ -1,54 +1,75 @@
 import java.io.File
-import java.io.BufferedReader
+import java.util.PriorityQueue
 
-class Celda(altura: Int, fila: Int, col: Int): Comparable<Celda> {
-    override fun compareTo(otro: Celda): Int = this.altura.compareTo(otro.altura)
+class Celda(val altura: Int, val fila: Int, val columna: Int): Comparable<Celda> {
+    override fun compareTo(other: Celda): Int = this.altura.compareTo(other.altura)
 }
 
-fun leerEntrada(input: String): {
-    val archivo = File(input)
-    if (!archivo.exists) {
-        println("El archivo $input no existe")
-        return null
-    }
-    
-    val bufferedReader: BufferedReader = archivo.bufferedReader()
-    val matriz = mutableListOf<IntArray>()
+object AlfonsoJose {
+    fun leerEntrada(input: String): Array<IntArray>? {
+        val archivo = File(input)
+        if (!archivo.exists()) {
+            println("El archivo $input no existe")
+            return null
+        }
 
-    var linea = bufferedReader.readLine()
-    while (linea != null) {
-        val fila = linea.trim().split("\\s+".toRegex()).map { it.toInt() }.toIntArray()
-        matriz.add(fila)
-        linea = bufferedReader.readLine()
-    }
-    bufferedReader.close()
-
-    return matriz.toTypedArray()
-}
-
-fun calcularAgua(matriz: Array<IntArray>): Int {
-    val n = matriz.size
-    val m = matriz[0].size
-    val visitado = Array(n) {BooleanArray(m)}
-    val cola = PriorityQueue<Celda>()
-
-    for (i in 0 until n) {
-        for (j in 0 until m) {
-            if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-                cola.add(Celda(matriz[i][j], i, j))
-                visitado[i][j] = true
+        val matriz = mutableListOf<IntArray>()
+        archivo.forEachLine { linea ->
+            if (linea.isNotBlank()) {
+                val fila = linea.trim().map { it.toString().toInt() }.toIntArray()
+                matriz.add(fila)
             }
         }
+        return matriz.toTypedArray()
     }
 
-    val contador = 0
+    fun calcularAgua(matriz: Array<IntArray>): Int {
+        val n = matriz.size
+        val m = matriz[0].size
+        val visitado = Array(n) { BooleanArray(m) }
+        val cola = PriorityQueue<Celda>()
 
-    while (cola.isNotEmpty()) {
-        val actual = cola.poll()
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    cola.add(Celda(matriz[i][j], i, j))
+                    visitado[i][j] = true
+                }
+            }
+        }
+
+        var totalAgua = 0
+        val direccionFila = intArrayOf(-1, 1, 0, 0)
+        val direccionColumna = intArrayOf(0, 0, -1, 1)
+
+        while (cola.isNotEmpty()) {
+            val actual = cola.poll()
+
+            for (i in 0 until 4) {
+                val nuevaFila = actual.fila + direccionFila[i]
+                val nuevaColumna = actual.columna + direccionColumna[i]
+
+                if (nuevaFila in 0 until n && nuevaColumna in 0 until m && !visitado[nuevaFila][nuevaColumna]){
+                    visitado[nuevaFila][nuevaColumna] = true
+
+                    if (matriz[nuevaFila][nuevaColumna] < actual.altura) {
+                        totalAgua += (actual.altura - matriz[nuevaFila][nuevaColumna])
+                    }
+
+                    val nuevoNivel = maxOf(matriz[nuevaFila][nuevaColumna], actual.altura)
+                    cola.add(Celda(nuevoNivel, nuevaFila, nuevaColumna))
+                }
+            }
+        }
+        return totalAgua
     }
-}
 
-fun main() {
-    val ciudad = leerEntrada("atlantis.txt")
-
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val ciudad = leerEntrada("atlantis.txt")
+        if (ciudad != null) {
+            val resultado = calcularAgua(ciudad)               
+            println(resultado)
+        }
+    }
 }
